280b703cf90ae7d4b8fc3d97c6f73461eb9dcf37
João Ricardo <joaoricardopratasilva@hotmail.com>
2021-06-30 12:38:23 +0100

    Add return on else

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 "use strick";
 
-const arrTest = [1, 3, 4, 5, 13, 20, 25, 40, 42, 53];
-const numTest = 40;
+const arrTest = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+    41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+];
+const numTest = 50;
 
 const biSearch = (num, arr) => {
     let ini = 0;
     let f = arr.length;
 
     let count = 0; // Big O purpose
+
+    console.log({ num });
 
     while (ini <= f) {
         let i = Math.floor((f + ini) / 2);
@@ -22,7 +28,7 @@ const biSearch = (num, arr) => {
             count++;
             ini = i + 1;
         } else {
-            console.log("Something is wrong!");
+            return console.log("Something is wrong!");
         }
     }
 };
