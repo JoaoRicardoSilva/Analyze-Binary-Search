3604ba64cdeafc5dc4967f616639c2c4b5a66ba1
João Ricardo <joaoricardopratasilva@hotmail.com>
2021-06-30 23:29:55 +0100

    Done: linearSearch()

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const arrTest = [
     22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
     41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
 ];
-const numTest = 50;
+const numTest = 1;
 
 const biSearch = (num, arr) => {
     let ini = 0;
@@ -32,4 +32,20 @@ const biSearch = (num, arr) => {
         }
     }
 };
-biSearch(numTest, arrTest);
+// biSearch(numTest, arrTest);
+
+const linearSearch = (num, arr) => {
+    let count = 0; // Big O purpose
+    let i = 0;
+
+    while (i <= arr.length - 1) {
+        if (arr[i] === num) {
+            console.log({ count });
+            console.log(arr[i]);
+        }
+        count++;
+        i++;
+    }
+};
+
+linearSearch(numTest, arrTest);
